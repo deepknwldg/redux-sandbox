59b03fa6092f9d04e041c7bc929b470c97f37c38
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-12 15:24:39 +0300

    add counter compnent

---
+++ src/components/counter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import { bindActionCreators } from "redux";
+
+const Counter = ({ counter, inc, dec, rnd }) => {
+  return (
+    <div className="jumbotron">
+      <h2 id="counter">{counter}</h2>
+      <button onClick={dec} id="dec" className="btn btn-primary btn-lg">
+        DEC
+      </button>
+      <button onClick={inc} id="inc" className="btn btn-primary btn-lg">
+        INC
+      </button>
+      <button onClick={rnd} id="rnd" className="btn btn-primary btn-lg">
+        RND
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
